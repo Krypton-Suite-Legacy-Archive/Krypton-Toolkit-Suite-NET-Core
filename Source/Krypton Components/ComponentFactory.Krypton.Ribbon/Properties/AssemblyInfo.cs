f335f7bc8e10ae7d520b15e683732a0e099035b1
Peter Wagner <peterwagner@live.co.uk>
2020-01-07 09:13:58 +0000

    * #20

--- Source/Krypton Components/ComponentFactory.Krypton.Ribbon/Properties/AssemblyInfo.cs
+++ Source/Krypton Components/ComponentFactory.Krypton.Ribbon/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿// *****************************************************************************
 // BSD 3-Clause License (https://github.com/ComponentFactory/Krypton/blob/master/LICENSE)
-//  © Component Factory Pty Ltd, 2006-2020, All rights reserved.
+//  © Component Factory Pty Ltd, 2006 - 2016, All rights reserved.
 // The software and associated documentation supplied hereunder are the 
 //  proprietary information of Component Factory Pty Ltd, 13 Swallows Close, 
 //  Mornington, Vic 3931, Australia and are supplied subject to license terms.
@@ -16,7 +16,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Security;
 
-[assembly: AssemblyCopyright("© Component Factory Pty Ltd, 2006-2020. Then modifications by Peter Wagner (aka Wagnerp) & Simon Coghlan (aka Smurf-IV) 2017-2020. All rights reserved.")]
+[assembly: AssemblyCopyright("© Component Factory Pty Ltd, 2006 - 2016. Then modifications by Peter Wagner (aka Wagnerp) & Simon Coghlan (aka Smurf-IV) 2017 - 2020. All rights reserved.")]
 [assembly: AssemblyDefaultAlias("ComponentFactory.Krypton.Ribbon.dll")]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: StringFreezing]
