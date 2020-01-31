35ea676f78fa8b4d2abe8ff4c33754a29d3f36c2
Peter Wagner <peterwagner@live.co.uk>
2020-01-31 10:46:40 +0000

    Update upload.cmd

--- upload.cmd
+++ upload.cmd
@@ -1,3 +1,33 @@
 @echo off
 
 :: This script is designed to upload new versions to NuGet. This will not work without the API key (UNDER CONSTRUCTION)
+
+
+:yes
+nuget push Krypton.Docking.5.500.2002.nupkg
+
+nuget push Krypton.Docking.Lite.5.500.2002.nupkg
+
+nuget push Krypton.Navigator.5.500.2002.nupkg
+
+nuget push Krypton.Navigator.Lite.5.500.2002.nupkg
+
+nuget push Krypton.Ribbon.5.500.2002.nupkg
+
+nuget push Krypton.Ribbon.Lite.5.500.2002.nupkg
+
+nuget push Krypton.Toolkit.5.500.2002.nupkg
+
+nuget push Krypton.Toolkit.Lite.5.500.2002.nupkg
+
+nuget push Krypton.Workspace.5.500.2002.nupkg
+
+nuget push Krypton.Workspace.Lite.5.500.2002.nupkg
+
+:no
+
+
+:deletepackages
+
+
+pause
