dac8dd186534adb5a1a1e0210c80f28a7cee0caf
Peter Wagner <peterwagner@live.co.uk>
2020-02-04 12:36:29 +0000

    * Files

---
+++ Source/Utilities/Project Migration Utility/Project Migration Utility/MainWindow.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace ProjectMigrationUtility
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtProjectDirectory = new System.Windows.Forms.TextBox();
+            this.btnBrowseProjectDirectory = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Project Directory:";
+            // 
+            // txtProjectDirectory
+            // 
+            this.txtProjectDirectory.Font = new System.Drawing.Font("Arial", 12F);
+            this.txtProjectDirectory.Location = new System.Drawing.Point(161, 9);
+            this.txtProjectDirectory.Name = "txtProjectDirectory";
+            this.txtProjectDirectory.Size = new System.Drawing.Size(734, 26);
+            this.txtProjectDirectory.TabIndex = 1;
+            // 
+            // btnBrowseProjectDirectory
+            // 
+            this.btnBrowseProjectDirectory.Font = new System.Drawing.Font("Arial", 12F);
+            this.btnBrowseProjectDirectory.Location = new System.Drawing.Point(901, 9);
+            this.btnBrowseProjectDirectory.Name = "btnBrowseProjectDirectory";
+            this.btnBrowseProjectDirectory.Size = new System.Drawing.Size(35, 26);
+            this.btnBrowseProjectDirectory.TabIndex = 2;
+            this.btnBrowseProjectDirectory.Text = ".&..";
+            this.btnBrowseProjectDirectory.UseVisualStyleBackColor = true;
+            this.btnBrowseProjectDirectory.Click += new System.EventHandler(this.btnBrowseProjectDirectory_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(16, 57);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(920, 100);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 12F);
+            this.button1.Location = new System.Drawing.Point(879, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(35, 26);
+            this.button1.TabIndex = 5;
+            this.button1.Text = ".&..";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Arial", 12F);
+            this.textBox2.Location = new System.Drawing.Point(172, 34);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(701, 26);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(18, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Backup Directory:";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(948, 590);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnBrowseProjectDirectory);
+            this.Controls.Add(this.txtProjectDirectory);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtProjectDirectory;
+        private System.Windows.Forms.Button btnBrowseProjectDirectory;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Source/Utilities/Project Migration Utility/Project Migration Utility/MainWindow.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectMigrationUtility
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowseProjectDirectory_Click(object sender, EventArgs e)
+        {
+            CommonOpenFileDialog cofd = new CommonOpenFileDialog();
+
+            cofd.IsFolderPicker = true;
+
+            cofd.Title = "Browse for a Project Directory:";
+
+            if (cofd.ShowDialog() == CommonFileDialogResult.Ok) txtProjectDirectory.Text = Path.GetFullPath(cofd.FileName);
+        }
+    }
+}
