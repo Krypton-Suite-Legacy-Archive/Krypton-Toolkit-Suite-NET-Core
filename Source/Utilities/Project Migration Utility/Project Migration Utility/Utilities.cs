ee8dc70538188e8a551fc9834a9fd09794d8cb0e
Peter Wagner <peterwagner@live.co.uk>
2020-02-05 13:02:29 +0000

    * Files

---
+++ Source/Utilities/Project Migration Utility/Project Migration Utility/Utilities.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+
+namespace ProjectMigrationUtility
+{
+    internal class Utilities
+    {
+        public Utilities()
+        {
+
+        }
+
+        public static void CompressFiles(string input, string output)
+        {
+            Compression.Compress.LZMA.Encoder encoder = new Compression.Compress.LZMA.Encoder();
+
+            using (FileStream fs = new FileStream(input, FileMode.Open))
+            {
+                using (FileStream stream = new FileStream(output, FileMode.Create))
+                {
+                    encoder.Code(fs, stream, -1, -1, null);
+
+                    stream.Flush();
+                }
+            }
+        }
+    }
+}
