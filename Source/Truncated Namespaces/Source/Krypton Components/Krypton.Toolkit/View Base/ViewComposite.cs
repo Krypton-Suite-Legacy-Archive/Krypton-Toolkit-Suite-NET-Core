edc50a5fc95187df17807806b24b545a55748bd2
Peter Wagner <peterwagner@live.co.uk>
2020-02-26 09:01:58 +0000

    Update ViewComposite.cs

--- Source/Truncated Namespaces/Source/Krypton Components/Krypton.Toolkit/View Base/ViewComposite.cs
+++ Source/Truncated Namespaces/Source/Krypton Components/Krypton.Toolkit/View Base/ViewComposite.cs
@@ -44,13 +44,17 @@ namespace Krypton.Toolkit
         protected override void Dispose(bool disposing)
         {
             // Dispose of all child views
-            while (Count > 0)
+            if (disposing)
             {
-                this[0].Dispose();
-                RemoveAt(0);
-            }
+                while (Count > 0)
+                {
+                    this[0].Dispose();
 
-            _views.Clear();
+                    RemoveAt(0);
+                }
+
+                _views.Clear();
+            }
             
             // Must call base class to finish disposing
             base.Dispose(disposing);
